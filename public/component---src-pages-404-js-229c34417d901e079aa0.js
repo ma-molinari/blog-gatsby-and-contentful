5d8f13e89f1034fecb79acd6c2ed7b4b344f4881
Matheus Molinari <ma.molinari@hotmail.com>
2020-02-24 14:18:02 -0300

    show public directory

---
+++ public/component---src-pages-404-js-229c34417d901e079aa0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{142:function(n,o,w){"use strict";w.r(o);o.default=function(){return"undefined"!=typeof window?window.location="/":null}}}]);
+//# sourceMappingURL=component---src-pages-404-js-229c34417d901e079aa0.js.map
